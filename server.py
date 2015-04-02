f06079c3ef9245535dec7a3a313f2473822667d5
alaypatel07 <alay1431@gmail.com>
2015-04-02 16:38:56 +0530

    /vote Endpoint added, not db interaction for now

--- server.py
+++ server.py
@@ -6,9 +6,10 @@ from tornado.ioloop import IOLoop
 from tornado.httpserver import HTTPServer
 from src.loginhandler import LoginHandler
 from src.votehandler import VoteHandler
+from tornado.options import options, define
 
+define('server_ip', type=str, default='172.31.40.214')
 
-server_ip = "127.0.0.1"
 
 app = Application([
     (r'/api/login', LoginHandler),
@@ -16,5 +17,6 @@ app = Application([
 ])
 
 server = HTTPServer(app)
-server.listen(8000, server_ip)
+print(options.server_ip)
+server.listen(8000, options.server_ip)
 IOLoop.instance().start()
