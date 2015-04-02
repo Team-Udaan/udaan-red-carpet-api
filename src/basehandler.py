0aaf5aaa02702d1afaf86207340d444fbf10e42a
alaypatel07 <alay1431@gmail.com>
2015-04-02 23:21:13 +0530

    added 130070107003 as voted id

--- src/basehandler.py
+++ src/basehandler.py
@@ -51,4 +51,5 @@ class BaseHandler(RequestHandler):
             self.set_header("Access-Control-Expose-Headers", "*")
             self.set_header("Access-Control-Allow-Methods", "Post, Options")
             self.set_header("Access-Control-Allow-Headers", "Accept, Content-Type")
+            print(response)
             self.finish(json.dumps(response))
