db92cd7355ea3076eda95d72ba7177e80d061bd9
alaypatel07 <alay1431@gmail.com>
2015-04-08 01:45:48 +0530

    For Public Reference

--- src/getfeedbackhandler.py
+++ src/getfeedbackhandler.py
@@ -8,7 +8,11 @@ class GetfeedbackHandler(BaseHandler):
     def get(self, *args, **kwargs):
         counter = self.client.get('counter')
         self.response['counter'] = counter
-        self.response['data'] = list()
+        self.response['data'] = {}
         for i in range(0, counter):
-            self.response['data'].append(self.client.hgetall('voter:' + str(i).decode('utf-8')))
+            data = self.client.hgetall('voter:' + str(i).decode('utf-8'))
+            for each_data in data:
+                data[each_data.decode('utf-8')] = data[each_data].decode('utf-8')
+                del data[each_data]
+                self.response['data']['voter:' + str(i)] = data
         self.send_error(200)
