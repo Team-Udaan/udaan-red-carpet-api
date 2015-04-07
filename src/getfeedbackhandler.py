56279ea6fb8d7e7a9772736ec7db8db9e18dffaf
alaypatel07 <alay1431@gmail.com>
2015-04-08 01:48:28 +0530

    For Public Display

--- src/getfeedbackhandler.py
+++ src/getfeedbackhandler.py
@@ -7,6 +7,7 @@ class GetfeedbackHandler(BaseHandler):
 
     def get(self, *args, **kwargs):
         counter = self.client.get('counter')
+        counter = counter.decode('utf-8')
         self.response['counter'] = counter
         self.response['data'] = {}
         for i in range(0, counter):
