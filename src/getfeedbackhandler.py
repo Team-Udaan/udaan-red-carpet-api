735f607d1db340084c1f85ff68b4e39104801d2b
alaypatel07 <alay1431@gmail.com>
2015-04-08 14:49:45 +0530

    getfeedback endpoint added

--- src/getfeedbackhandler.py
+++ src/getfeedbackhandler.py
@@ -11,6 +11,10 @@ class GetfeedbackHandler(BaseHandler):
         self.response['counter'] = counter
         self.response['data'] = {}
         for i in range(0, int(counter)):
-            data = self.client.hgetall('voter:' + str(i))
-            self.response['data']['voter:' + str(i)] = data
+            voter = 'voter:' + str(i)
+            self.response['data'][voter] = {}
+            data = self.client.hgetall(voter)
+            for each_data in data:
+                key = each_data.decode('utf-8')
+                self.response['data'][voter][key] = data[each_data].decode('utf-8')
         self.send_error(200)
